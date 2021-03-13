df2f6d1ffdf83294ced1533548d0f26e240099c6
fun1355 <fun1355@users.noreply.github.com>
2021-03-13 12:04:29 +0800

    make it work on Linux, 1.grep \( \); 2.when resource undownload but on the download queue, there exsit json config, should escape.

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 import subprocess
 
 def getTitle(partPath):
-    if os.path.exists(partPath + "\\entry.json"):
-        with open(partPath + "\\entry.json", 'r', encoding='utf-8') as file:
+    if os.path.exists(partPath + "/entry.json"):
+        with open(partPath + "/entry.json", 'r', encoding='utf-8') as file:
             ret_dic = json.load(file)
             title = ret_dic['title']
         return formatTitle(title)
@@ -19,27 +19,37 @@ def formatTitle(partTitle):
     partTitle = partTitle.replace("⑥", "6")
     partTitle = partTitle.replace("&", "-")
     partTitle = partTitle.replace(".", "_")
+    partTitle = partTitle.replace("（", "")
+    partTitle = partTitle.replace("）", "")
+    partTitle = partTitle.replace("(", "")
+    partTitle = partTitle.replace(")", "")
+    partTitle = partTitle.replace("-", "")
     return partTitle
 
 def extracVideo(partPath, outputPath):
-    if not os.path.exists(partPath + "\\entry.json"):
+    if not os.path.exists(partPath + "/entry.json"):
         return
 
-    with open(partPath + "\\entry.json", 'r', encoding='utf-8') as file:
+    with open(partPath + "/entry.json", 'r', encoding='utf-8') as file:
             ret_dic = json.load(file)
+            print("ret_dic:" + str(ret_dic))
+            print("partPath" + partPath)
+            if ("page_data" not in ret_dic) or ("type_tag" not in ret_dic) or \
+               ("title" not in ret_dic):
+                return
             page_data = ret_dic['page_data']
-            type_tag =ret_dic['type_tag']
+            type_tag = ret_dic['type_tag']
             videoTitle = ret_dic['title']
             partTitle = page_data['part']
 
     outPutVideoName = formatTitle(partTitle) + ".mp4"
-    outputVideoPath = outputPath + "\\" + outPutVideoName
+    outputVideoPath = outputPath + "/" + outPutVideoName
 
-    fileRealPath = partPath + "\\" + type_tag
-    oldAudioPath = fileRealPath + "\\" + "audio.m4s"
-    oldVideoPath = fileRealPath + "\\" + "video.m4s"
-    newAudioPath = fileRealPath + "\\" + "audio.mp3"
-    newVideoPath = fileRealPath + "\\" + "video.mp4"
+    fileRealPath = partPath + "/" + type_tag
+    oldAudioPath = fileRealPath + "/" + "audio.m4s"
+    oldVideoPath = fileRealPath + "/" + "video.m4s"
+    newAudioPath = fileRealPath + "/" + "audio.mp3"
+    newVideoPath = fileRealPath + "/" + "video.mp4"
     if os.path.exists(oldAudioPath):
         os.rename(oldAudioPath, newAudioPath)
     if os.path.exists(oldVideoPath):
@@ -50,21 +60,19 @@ def extracVideo(partPath, outputPath):
     print(outputVideoPath)
     subprocess.call("ffmpeg " + "-i " + newVideoPath + " -i "+ newAudioPath + " -codec copy " + outputVideoPath, shell=True)
 
-rootPath = os.getcwd() + "\\resource"
-outputPath = os.getcwd() + "\\output"
+rootPath = os.getcwd() + "/resource"
+outputPath = os.getcwd() + "/output"
 
 for videoPath in os.listdir(rootPath):
-    videoPath = rootPath + "\\" + videoPath
-    if not os.path.isdir(videoPath) :
-        continue
+    videoPath = rootPath + "/" + videoPath
     parts = os.listdir(videoPath)
     if len(parts) < 1 :
         continue
-    title = getTitle(videoPath + "\\" + parts[0])
-    videoOutputPath = outputPath + "\\" + title;
+    title = getTitle(videoPath + "/" + parts[0])
+    videoOutputPath = outputPath + "/" + title;
     if not os.path.exists(videoOutputPath):
         os.makedirs(videoOutputPath)
 
     for partPath in parts:
-        partPath = videoPath + "\\" + partPath;
+        partPath = videoPath + "/" + partPath;
         extracVideo(partPath, videoOutputPath)
