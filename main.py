33eb5d4412ac6fdc9e4049318729260cd9cc7af1
fun1355 <1174433+fun1355@users.noreply.github.com>
2021-03-24 19:45:32 +0800

    Update main.py
    1.remove the Linux version, make the code universal b/t posix and
        Windows OS, with the help of libpath.
    2.make code style unified to camelStyle, make comments into English
        as README.md does.
    3.make 'formatTitle' function tidy and tiny.
    4.make Windows and posix separate resourse/output path, bc I don't
        have Windows enviroment and I don't know how to set softlink-like
        command under Windows.

--- main.py
+++ main.py
@@ -1,93 +1,110 @@
 import json
 import os
 import subprocess
+# https://medium.com/@ageitgey/python-3-quick-tip-the-easy-way-to-deal-with-file-paths-on-windows-mac-and-linux-11a072b58d5f
+# https://docs.python.org/3/library/pathlib.html#concrete-paths
+# use Path to make code universal between posix and Windows OS.
+from pathlib import Path
 
 def getTitle(partPath):
-    if os.path.exists(partPath + "\\entry.json"):
-        with open(partPath + "\\entry.json", 'r', encoding='utf-8') as file:
-            ret_dic = json.load(file)
-            title = ret_dic['title']
+    partPath = Path(partPath)
+    if os.path.exists(partPath / "entry.json"):
+        with open(partPath / "entry.json", 'r', encoding='utf-8') as file:
+            retDic = json.load(file)
+            title = retDic['title']
         return formatTitle(title)
 
 def formatTitle(partTitle):
-    partTitle = partTitle.replace(" ", "")
-    partTitle = partTitle.replace("①", "1")
-    partTitle = partTitle.replace("②", "2")
-    partTitle = partTitle.replace("③", "3")
-    partTitle = partTitle.replace("④", "4")
-    partTitle = partTitle.replace("⑤", "5")
-    partTitle = partTitle.replace("⑥", "6")
-    partTitle = partTitle.replace("&", "-")
-    partTitle = partTitle.replace(".", "_")
+    replaceDict = {" ": "", "①": "1", "②": "2", "③": "3", "④": "4", "⑤": "5", "⑥": "6",
+                   "&": "-", ".": "_", "（": "", "）": "", "(": "", ")": "", "-": "", "/": "_"}
+    for key, value in replaceDict.items():
+        partTitle = partTitle.replace(key, value)
+    print("partTitle :" + str(partTitle))
     return partTitle
 
 def extracVideo(partPath, outputPath):
-    if not os.path.exists(partPath + "\\entry.json"):
+    partPath = Path(partPath)
+    outputPath = Path(outputPath)
+    if not os.path.exists(partPath / "entry.json"):
         return
 
-    with open(partPath + "\\entry.json", 'r', encoding='utf-8') as file:
-            ret_dic = json.load(file)
-            page_data = ret_dic['page_data']
-            type_tag =ret_dic['type_tag']
-            videoTitle = ret_dic['title']
-            partTitle = page_data['part']
-            width = page_data['width']
-            height = page_data['height']
-            
+    with open(partPath / "entry.json", 'r', encoding='utf-8') as file:
+            retDic = json.load(file)
+            print("retDic:" + str(retDic))
+            print("partPath:" + str(partPath))
+            if ("page_data" not in retDic) or ("type_tag" not in retDic) or \
+               ("title" not in retDic):
+                return
+            pageData = retDic['page_data']
+            typeTag = retDic['type_tag']
+            videoTitle = retDic['title']
+            partTitle = pageData['part']
+            width = pageData['width']
+            height = pageData['height']
+
     formatPartTitle = formatTitle(partTitle)
-    outPutVideoName = formatPartTitle + ".mp4"
-    outputVideoPath = outputPath + "\\" + outPutVideoName
+    # https://docs.python.org/3/library/pathlib.html#pathlib.PurePath.with_suffix
+    # PurePath.with_suffix(suffix)
+    # Return a new path with the suffix changed. If the original path doesn’t have a suffix, the new suffix is appended instead. If the suffix is an empty string, the original suffix is removed:
+    outPutVideoName = Path(formatPartTitle).with_suffix(".mp4")
+    outputVideoPath = outputPath / outPutVideoName
 
-    #提取弹幕文件
-    if os.path.exists(partPath + "\\danmaku.xml"):
-        outAssName = outputPath + "\\" + formatPartTitle + ".ass"
-        print(outAssName)
+    #extract danmu file
+    if os.path.exists(partPath / "danmaku.xml"):
+        outAssName = (outputPath / formatPartTitle).with_suffix(".ass")
+        print(str(outAssName))
         if os.path.exists(outAssName):
-            print(outAssName + "已提取")
+            print(str(outAssName) + "extract down.")
         else:
-            import danmaku2ass #利用别人写好的danmaku2ass.py中的方法
+            import danmaku2ass # reuse code in danmaku2ass.py
 
-            #高分辨率下，字体大一些
+            # when high resolution, make font larger
             fontSize = 23.0
             if width >= 1920:
                 fontSize = 36.0
 
-            danmaku2ass.Danmaku2ASS(partPath + "\\danmaku.xml","Bilibili" , outAssName, width, height,\
-                reserve_blank=480,font_size=fontSize, text_opacity=1, duration_marquee=12.0, duration_still=6.0) #这些参数可调
+            danmaku2ass.Danmaku2ASS(str(partPath / "danmaku.xml"), "Bilibili" , str(outAssName), width, height,\
+                reserve_blank=480,font_size=fontSize, text_opacity=1, duration_marquee=12.0, duration_still=6.0) # to tune argus
 
-    fileRealPath = partPath + "\\" + type_tag
-    oldAudioPath = fileRealPath + "\\" + "audio.m4s"
-    oldVideoPath = fileRealPath + "\\" + "video.m4s"
-    newAudioPath = fileRealPath + "\\" + "audio.mp3"
-    newVideoPath = fileRealPath + "\\" + "video.mp4"
+
+    fileRealPath = partPath / typeTag
+    oldAudioPath = fileRealPath / "audio.m4s"
+    oldVideoPath = fileRealPath / "video.m4s"
+    newAudioPath = fileRealPath / "audio.mp3"
+    newVideoPath = fileRealPath / "video.mp4"
+
     if os.path.exists(oldAudioPath):
         os.rename(oldAudioPath, newAudioPath)
     if os.path.exists(oldVideoPath):
         os.rename(oldVideoPath, newVideoPath)
     if os.path.exists(outputVideoPath):
-        print(outputVideoPath + "已提取")
+        print(str(outputVideoPath) + " extract done!")
         return
     print(outputVideoPath)
-    subprocess.call("ffmpeg " + "-i " + newVideoPath + " -i "+ newAudioPath + " -codec copy " + outputVideoPath, shell=True)
+    # https://docs.python.org/3/library/subprocess.html
+    # https://stackoverflow.com/questions/25655173/does-pythons-subprocess-popen-accept-spaces-in-paths
+    # A string surrounded by double quotation marks is interpreted as a single argument, regardless of white space contained within. A quoted string can be embedded in an argument.
+    subprocess.call('ffmpeg -i "%s" -i "%s" -codec copy "%s"' %(str(newVideoPath), str(newAudioPath), str(outputVideoPath)), shell=True)
 
+if os.name == "nt":
+    rootPath = Path(os.getcwd()) / "resource"
+    outputPath = Path(os.getcwd()) / "output"
+else: # "posix"
+    rootPath = Path(os.getcwd()) / "resource_linux"
+    outputPath = Path(os.getcwd()) / "output_linux"
 
-#begin
-rootPath = os.getcwd() + "\\resource"
-outputPath = os.getcwd() + "\\output"
 
 for videoPath in os.listdir(rootPath):
-    videoPath = rootPath + "\\" + videoPath
-    if not os.path.isdir(videoPath) :
-        continue
+    videoPath = rootPath / videoPath
     parts = os.listdir(videoPath)
     if len(parts) < 1 :
         continue
-    title = getTitle(videoPath + "\\" + parts[0])
-    videoOutputPath = outputPath + "\\" + title;
+    title = getTitle(videoPath / parts[0])
+    videoOutputPath = outputPath / str(title);
     if not os.path.exists(videoOutputPath):
         os.makedirs(videoOutputPath)
 
     for partPath in parts:
-        partPath = videoPath + "\\" + partPath;
+        partPath = videoPath / partPath;
         extracVideo(partPath, videoOutputPath)
 
